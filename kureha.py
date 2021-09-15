b704d87b8813a7758b427d224766bed6fbab414c
Lucas <yamcha.rulesmc@gmail.com>
2021-09-15 13:35:36 -0400

    Update kureha.py

--- kureha.py
+++ kureha.py
@@ -1,37 +1,40 @@
-import discord
+#Kureha Discord Bot
+import discord #pip install -U discord.py
 import logging
-import nest_asyncio
+import nest_asyncio #https://github.com/erdewit/nest_asyncio
 
 logging.basicConfig(level=logging.INFO)
 nest_asyncio.apply()
 kver = '0.1'
 kverdt = '\n9/15/21'
 
-TOKEN='Token'
-
+TOKEN='ODQ1NzYyNDU1MDg1OTA3OTkx.YKlrqg.6HE2C7ZS0lgmyK81SZ3dAXxCcv0'
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Bot user {0.user} is online'.format(client))
+    print('Bot user {} is online'.format(client.user))
     
 @client.event
 async def on_message(message):
-    if str(message.author) == 'nkdv#4125':
+    # if str(message.author) == 'nkdv#4125':
             
-        if message.content.startswith('ut04'):
-            await message.channel.send('Red power node Isolated!')
+    #     if message.content == 'ut04':
+    #         await message.channel.send('Red power node Isolated!')
             
-        elif message.content.startswith('e1m1'):
-            await message.channel.send('Until it is done.')
+    #     elif message.content == 'e1m1':
+    #         await message.channel.send('Until it is done.')
             
     if message.content == 'kd':
         await message.channel.send('Hey! Thats my command prefix!')
             
-    elif message.content.startswith('kdversion'):
-        await message.channel.send('Version ' + kver + kverdt)
+    elif message.content == 'kdver' or 'kdversion':
+        await message.channel.send('Kureha is running version ' + kver + kverdt)
     
-    elif message.content.startswith('kdhelp'):
-        await message.channel.send('https://puu.sh/IaRyY/46a81cad2f.png')
+    elif message.content == 'kdhelp':
+        await message.channel.send('Check out the commands page on github: https://github.com/nkdv0/KurehaBot/blob/main/Commands')
          
+    elif message.content == 'kd':
+        ()
+    
 client.run(TOKEN)
