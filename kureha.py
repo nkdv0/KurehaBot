00937c2e78f07f236b29adf5fb740518de56f92f
Lucas <37598838+nkdv0@users.noreply.github.com>
2021-09-15 12:07:05 -0400

    Add files via upload

---
+++ kureha.py
@@ -0,0 +1,37 @@
+import discord
+import logging
+import nest_asyncio
+
+logging.basicConfig(level=logging.INFO)
+nest_asyncio.apply()
+kver = '0.1'
+kverdt = '\n9/15/21'
+
+TOKEN='ODQ1NzYyNDU1MDg1OTA3OTkx.YKlrqg.p_UKOjni9WcgKLjK8POZ0X5rcxE'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Bot user {0.user} is online'.format(client))
+    
+@client.event
+async def on_message(message):
+    if str(message.author) == 'nkdv#4125':
+            
+        if message.content.startswith('ut04'):
+            await message.channel.send('Red power node Isolated!')
+            
+        elif message.content.startswith('e1m1'):
+            await message.channel.send('Until it is done.')
+            
+    if message.content == 'kd':
+        await message.channel.send('Hey! Thats my command prefix!')
+            
+    elif message.content.startswith('kdversion'):
+        await message.channel.send('Version ' + kver + kverdt)
+    
+    elif message.content.startswith('kdhelp'):
+        await message.channel.send('https://puu.sh/IaRyY/46a81cad2f.png')
+         
+client.run(TOKEN)
