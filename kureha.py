95da6f58f0f4135c1f7aa84dbcf8f16c2a521e01
Lucas <yamcha.rulesmc@gmail.com>
2021-09-15 14:52:33 -0400

    Update kureha.py

--- kureha.py
+++ kureha.py
@@ -6,7 +6,7 @@ import nest_asyncio #https://github.com/erdewit/nest_asyncio
 logging.basicConfig(level=logging.INFO)
 nest_asyncio.apply()
 kver = '0.1'
-kverdt = '\n9/15/21'
+kverdt = '9/15/21'
 
 TOKEN=''
 client = discord.Client()
@@ -14,9 +14,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('Bot user {} is online'.format(client.user))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for kd prefix"))
     
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     # if str(message.author) == 'nkdv#4125':
             
     #     if message.content == 'ut04':
@@ -26,15 +29,20 @@ async def on_message(message):
     #         await message.channel.send('Until it is done.')
             
     if message.content == 'kd':
-        await message.channel.send('Hey! Thats my command prefix!')
+        await message.channel.send('Hey! Thats my command prefix! For help, try kdhelp or kdcommands!')
             
-    elif message.content == 'kdver' or 'kdversion':
-        await message.channel.send('Kureha is running version ' + kver + kverdt)
+    elif message.content == 'kdver' or message.content == 'kdversion':
+        await message.channel.send('Kureha is running.\nVersion ' + kver + '\nVersion date ' + kverdt)
     
     elif message.content == 'kdhelp':
-        await message.channel.send('Check out the commands page on github: https://github.com/nkdv0/KurehaBot/blob/main/Commands')
+        await message.channel.send('Check out the commands or other information on github: https://github.com/nkdv0/KurehaBot')
          
-    elif message.content == 'kd':
-        ()
+    elif message.content == 'kdcommands':
+        await message.channel.send('https://github.com/nkdv0/KurehaBot/blob/main/Commands')
+    
+    elif message.content.startswith('kd'):
+        await message.channel.send('Unknown command! Try kdhelp or kdcommands!')
+    else:
+        return
     
 client.run(TOKEN)
