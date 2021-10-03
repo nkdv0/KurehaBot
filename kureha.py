ff5aa09e04f87a90bf5b33b23d47da20f653aed9
Lucas <yamcha.rulesmc@gmail.com>
2021-10-02 20:29:11 -0400

    Release v0.2.0a

--- kureha.py
+++ kureha.py
@@ -5,29 +5,22 @@ import nest_asyncio #https://github.com/erdewit/nest_asyncio
 
 logging.basicConfig(level=logging.INFO)
 nest_asyncio.apply()
-kver = 'A0.1'
-kverdt = '9/15/21'
+kver = 'v0.2.0a'
+kverdt = '10/2/21'
 
-TOKEN=''
+TOKEN='' #REMEMBER TO REMOVE THIS BEFORE MAKING A COMMIT
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Bot user {} is online'.format(client.user))
+    print('{} is online'.format(client.user))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for kd prefix"))
     
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    # if str(message.author) == 'nkdv#4125':
-            
-    #     if message.content == 'ut04':
-    #         await message.channel.send('Red power node Isolated!')
-            
-    #     elif message.content == 'e1m1':
-    #         await message.channel.send('Until it is done.')
-            
+    
     if message.content == 'kd':
         await message.channel.send('Hey! Thats my command prefix! For help, try kdhelp or kdcommands!')
             
@@ -35,13 +28,19 @@ async def on_message(message):
         await message.channel.send('Kureha is running.\nVersion ' + kver + '\nVersion date ' + kverdt)
     
     elif message.content == 'kdhelp':
-        await message.channel.send('Check out the commands or other information on github: https://github.com/nkdv0/KurehaBot')
+        await message.channel.send('Check out the commands list or other information on github: https://github.com/nkdv0/KurehaBot')
          
-    elif message.content == 'kdcommands':
+    elif message.content == 'kdcommands' or message.content == 'kdc':
         await message.channel.send('https://github.com/nkdv0/KurehaBot/blob/main/Commands')
     
     elif message.content.startswith('kd'):
         await message.channel.send('Unknown command! Try kdhelp or kdcommands!')
+        
+    elif message.contet == 'kdgit':
+        await message.channel.send('https://github.com/nkdv0/KurehaBot')
+        
+    
+        
     else:
         return
     
